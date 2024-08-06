6f94b00dd4f1253c73faf3bd3926421564cb5a5a
William Bojczuk <williambojczuk@gmail.com>
2024-08-06 09:05:51 -0400

    init

---
+++ src/app/(pages)/(home)/page.tsx
@@ -0,0 +1,15 @@
+
+
+export default function Home() {
+  return (
+    <>
+      <div style={{height: "90vh"}} className="center">
+        <h1 style={{
+          textAlign: "center",
+          fontSize: "3rem",
+          color: "var(--secondary-text-color)"
+        }}><span className="highlight">Anytime</span> Roadside Services <br /> Coming Soon!</h1>
+      </div>
+    </>
+  )
+}
