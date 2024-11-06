95976ef3eb17c35ec16b5a397b2d41e80752dac3
William Bojczuk <williambojczuk@gmail.com>
2024-11-06 17:37:32 -0500

    updated

--- src/app/(mainsite)/components/contactpage/FormAndMap/FormAndMap.tsx
+++ src/app/(mainsite)/components/contactpage/FormAndMap/FormAndMap.tsx
@@ -11,7 +11,7 @@ return (
     <div className={styles.mapWrapper}>
         <div className={styles.menuCover} >
         
-        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2973.2354173150925!2d-80.9399157!3d41.8232286!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x88318d517b26da01%3A0x6cf28ecc41bbca12!2s3583%20Austin%20Rd%2C%20Geneva%2C%20OH%2044041!5e0!3m2!1sen!2sus!4v1730462631735!5m2!1sen!2sus" width="600" height="450" style={{border: 0}}  loading="lazy" ></iframe>
+        <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d672.7993897682142!2d-80.9351653!3d41.8095585!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x88318c31d18a2ea7%3A0xecffc86608472481!2s745%20E%20Main%20St%20%232%2C%20Geneva%2C%20OH%2044041!5e1!3m2!1sen!2sus!4v1730932612701!5m2!1sen!2sus" width="600" height="450" style={{border: 0}}  loading="lazy" ></iframe>
         </div>
     </div>
  </div>
