a765286a33c2e9aa2e5130eca9efa5c2eae499f8
William Bojczuk <williambojczuk@gmail.com>
2024-11-19 12:16:08 -0500

    updated

--- src/app/(mainsite)/components/homepage/Header/Header.tsx
+++ src/app/(mainsite)/components/homepage/Header/Header.tsx
@@ -9,6 +9,8 @@ return (
 
     <h1>Your Roadside<br /> Services Go-To</h1>
 
+    <h2>We take Cashapp, Venmo, and Paypal<span className="highlight">!</span></h2>
+
     <p>Serving all of your roadside needs in Geneva Ohio and surrounding areas, assistance is only <span className="underline"><strong>one call away!</strong></span></p>
 
     <div className={`center main-body ${styles.linkWrapper}`}>
