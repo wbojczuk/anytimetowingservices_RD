070f1d73ed5794a1aea7d9425c135bf4ae7b1515
William Bojczuk <williambojczuk@gmail.com>
2024-12-30 06:22:18 -0500

    updated

--- src/app/(mainsite)/components/misc/Footer/Footer.tsx
+++ src/app/(mainsite)/components/misc/Footer/Footer.tsx
@@ -71,6 +71,11 @@ export default function Footer() {
                   <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24"><path fill="currentColor" d="M6.62 10.79c1.44 2.83 3.76 5.15 6.59 6.59l2.2-2.2c.28-.28.67-.36 1.02-.25c1.12.37 2.32.57 3.57.57a1 1 0 0 1 1 1V20a1 1 0 0 1-1 1A17 17 0 0 1 3 4a1 1 0 0 1 1-1h3.5a1 1 0 0 1 1 1c0 1.25.2 2.45.57 3.57c.11.35.03.74-.25 1.02z"></path></svg>
                   <a href={"tel:" + process.env.NEXT_PUBLIC_PHONE_NUM} target="_blank">{process.env.NEXT_PUBLIC_PHONE_NUM_FORMATTED}</a>
                 </aside>
+
+                <aside className={`${styles.contactItem} ${styles.listItem}`}>
+                <svg xmlns="http://www.w3.org/2000/svg" width={24} height={24} viewBox="0 0 24 24"><path fill="currentColor" d="M2 22V4q0-.825.588-1.412T4 2h16q.825 0 1.413.588T22 4v12q0 .825-.587 1.413T20 18H6zm4-8h8v-2H6zm0-3h12V9H6zm0-3h12V6H6z"></path></svg>
+                  <a href={"sms:" + process.env.NEXT_PUBLIC_TEXT_NUM} target="_blank">TEXTING: {process.env.NEXT_PUBLIC_TEXT_NUM_FORMATTED}</a>
+                </aside>
                 
 
                 {/* <aside className={`${styles.contactItem} ${styles.listItem}`}>
