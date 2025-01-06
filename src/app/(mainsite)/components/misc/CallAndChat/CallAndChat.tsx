38580b140b44cf02acbc834eb4f841f6240cbe4c
William Bojczuk <williambojczuk@gmail.com>
2025-01-06 07:36:37 -0500

    upodate

--- src/app/(mainsite)/components/misc/CallAndChat/CallAndChat.tsx
+++ src/app/(mainsite)/components/misc/CallAndChat/CallAndChat.tsx
@@ -21,7 +21,7 @@ return (
     <a href={"tel:" + process.env.NEXT_PUBLIC_PHONE_NUM} target="_blank">
         <img src="/img/call.webp" aria-hidden />
     </a>
-    <Link target="_blank" href={"sms:" + process.env.NEXT_PUBLIC_TEXT_NUM}>
+    <Link href={"/contact"}>
         <img src="/img/chat.webp" aria-hidden />
     </Link>
 
